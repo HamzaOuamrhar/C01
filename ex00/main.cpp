0a4227654432bddae90f972ae39c79e3522e50a7
HamzaOuamrhar <hamzaouamrhar09@gmail.com>
2024-10-01 11:44:33 +0100

    1337

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: houamrha <houamrha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 16:47:19 by houamrha          #+#    #+#             */
-/*   Updated: 2024/09/24 17:16:37 by houamrha         ###   ########.fr       */
+/*   Updated: 2024/10/01 11:44:16 by houamrha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,13 @@
 
 int main()
 {
-	Zombie *z = newZombie("Booo");
-	z->announce();
-	delete z;
+	Zombie *b = newZombie("Booo");
+	b->announce();
+
+	Zombie z("Zooo");
+	z.announce();
+
+	randomChump("Kooo");
+
+	delete b;
 }
